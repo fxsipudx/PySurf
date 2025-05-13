1ed11a7e07ba0c3aa1311d7c59fee6138d795b3f
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2025-05-13 23:32:27 +0200

    built URL checker

---
+++ main.py
@@ -0,0 +1,12 @@
+class URL:
+    def __init__(self,url):
+        self.scheme, url = url.split("://",1)
+        assert self.scheme == "http"
+        
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/",1)
+        self.path = "/" + url
+        
+            
+        
