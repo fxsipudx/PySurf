e6486b262d8ff91078d57ea7377dd06d5343a60a
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2025-05-15 01:24:12 +0200

    Socket Setup + Connection

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 
 class URL:
-    def __init__(self,url):
+    def __init__(self,url): #The URL Parser Splits http://example.org/index.html
         self.scheme, url = url.split("://",1)
         assert self.scheme == "http" # Helps us in debugging. If "True" nothing happens if false cause problem
         
@@ -10,9 +10,12 @@ class URL:
         self.host, url = url.split("/",1)
         self.path = "/" + url
         
-    def response(self):
+    def response(self): #Socket Setup + Connection
         s = socket.socket(
             family=socket.AF_INET, # Using IPv4 addresses 
             type=socket.SOCK_STREAM, # Setting up a stream socket (TCP)
             proto=socket.IPPROTO_TCP  
         )
+        s.connect((self.host,80))
+    
+   
