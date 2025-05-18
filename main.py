5f5a3aba2beb3c460627d6d80250274e27fdb18b
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2025-05-18 23:18:48 +0200

    simplify the code

--- main.py
+++ main.py
@@ -12,14 +12,9 @@ class URL:
         self.path = "/" + url
         
     def request(self): #Socket Setup + Connection
-        s = socket.socket(
-            family=socket.AF_INET, # Using IPv4 addresses 
-            type=socket.SOCK_STREAM, # Setting up a stream socket (TCP)
-            proto=socket.IPPROTO_TCP  
-        )
-        # Create a TCP socket and connect to the host
-        s.connect((self.host,80))
         
+        s = socket.create_connection((self.host, 80))
+               
         # Compose HTTP GET request
         request = f"GET {self.path} HTTP/1.0\r\nHost: {self.host}\r\n\r\n"
         s.send(request.encode("utf8"))
