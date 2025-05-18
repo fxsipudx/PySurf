e49cd6d331275853ce8bc48548c220836fa0c8a2
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2025-05-18 22:42:01 +0200

    Handle 301/302 redirects Handle chunked or gzip-encoded responses

--- main.py
+++ main.py
@@ -7,15 +7,49 @@ class URL:
         
         if "/" not in url:
             url = url + "/"
+            
         self.host, url = url.split("/",1)
         self.path = "/" + url
         
-    def response(self): #Socket Setup + Connection
+    def request(self): #Socket Setup + Connection
         s = socket.socket(
             family=socket.AF_INET, # Using IPv4 addresses 
             type=socket.SOCK_STREAM, # Setting up a stream socket (TCP)
             proto=socket.IPPROTO_TCP  
         )
+        # Create a TCP socket and connect to the host
         s.connect((self.host,80))
+        
+        # Compose HTTP GET request
+        request = f"GET {self.path} HTTP/1.0\r\nHost: {self.host}\r\n\r\n"
+        s.send(request.encode("utf8"))
+        
+        response = s.makefile("r",encoding="utf8",newline="\r\n")
+        status_line = response.readline()
+        version,status,explanation = status_line.split(" ",2)
+        
+        response_headers = {}
+        
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header,value = line.split(":",1)
+            response_headers[header.casefold()] = value.strip()
+        
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        
+        content = response.read()
+        s.close()
+        return content        
+            
+            
+        
+        
+        
+    
+        
+        
     
    
